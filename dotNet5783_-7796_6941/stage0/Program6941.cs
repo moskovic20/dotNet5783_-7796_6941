6ee184d67c56731b213ac7018320f02ec73e8dd3
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2022-10-25 19:09:17 +0300

    Welcome the second partner

---
+++ dotNet5783_-7796_6941/stage0/Program6941.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program
+    {
+        static partial void Welcome6941();
+
+        static partial void Welcome6941()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}

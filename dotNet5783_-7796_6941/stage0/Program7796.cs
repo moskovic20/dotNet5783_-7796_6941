2d70f241c37cf698edf3b98011c122eb902cbd98
hilaaiz <haizenba@g.jct.ac.il>
2022-10-25 18:25:36 +0300

    initial code + Welcome function

---
+++ dotNet5783_-7796_6941/stage0/Program7796.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace stage0
+{
+    partial class Progrem
+    {
+        static void Main(string[] args)
+        {
+            welcome7796();
+            welcome6941();
+            Console.ReadKey();
+        }
+
+        static partial void welcome6941();
+        private static void welcome7796()
+        {
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}

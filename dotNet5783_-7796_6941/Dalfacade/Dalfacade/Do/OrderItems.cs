fb0487a5063562226274e7e344fb1338974f1ea0
hilaaiz <haizenba@g.jct.ac.il>
2022-11-01 22:34:04 +0200

    structors for dal

--- dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/OrderItems.cs
+++ dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/OrderItems.cs
@@ -2,6 +2,14 @@
 
 public struct OrderItems
 {
+    
+    public int ID { get; set; }
 
+    public int? IDOfOrdet { get; set; }
+    public int? IDOfProduct { get; set; }
+    
+    public float? priceOfOneItem { get; set; }
+
+    public int? amountOfItem { get; set; }
 }
 
--- dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/Product.cs
+++ dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/Product.cs
@@ -1,9 +1,31 @@
 ﻿namespace Do;
-
+/// <summary>
+/// structor for all kinds of products in our book store
+/// </summary>
 public struct Product
 {
-    public string? nameOfBook { get; set; }
-    public int ID { get; set; }
-   
+    /// <summary>
+    /// uniqe name of the current book
+    /// </summary>
+    public string? nameOfProduct { get; set; }
 
+    /// <summary>
+    /// ID of this product
+    /// </summary>
+    public int ID { get; set; }
+
+    /// <summary>
+    /// for category of this book or other items 
+    /// </summary>
+    public string? Category { get; set; }
+
+    /// <summary>
+    /// price of this product
+    /// </summary>
+    public float? Price { get; set; }
+
+    /// <summary>
+    /// amount of this product on shop
+    /// </summary>
+    public int? Amount{get; set;}
 }

307fd8110d13a6eb47d936df282ae28503353fff
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2022-11-03 11:23:04 +0200

    orterItem

--- dotNet5783_-7796_6941/DaLFacade/DaLFacade/DO/OrderItems.cs
+++ dotNet5783_-7796_6941/DaLFacade/DaLFacade/DO/OrderItems.cs
@@ -1,16 +1,24 @@
-﻿ namespace Do;
-
+﻿namespace Do;
 public struct OrderItems
 {
-    
     public int ID { get; set; }
 
-    public int? IDOfOrdet { get; set; }
+    public int? IdOfOrder { get; set; }
 
-    public int? IDOfProduct { get; set; }
-    
+    public int? IdOfProduct { get; set; }
+
     public float? priceOfOneItem { get; set; }
 
     public int? amountOfItem { get; set; }
+
+
+    public override string ToString() => $@"
+	   Ordered product ID : {ID}, 
+	   Order ID (Customer and shipping details) : {IdOfOrder}
+       Product ID : {IdOfProduct}
+       Price of one item : {priceOfOneItem}
+       the amount of items : {amountOfItem}
+	";
+
 }
 

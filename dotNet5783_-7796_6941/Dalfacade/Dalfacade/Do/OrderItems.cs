44b00989e9b76dcad3a8a920479c4e5af671de4b
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2022-11-03 11:22:29 +0200

    order and orderItem

--- dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/OrderItems.cs
+++ dotNet5783_-7796_6941/Dalfacade/Dalfacade/Do/OrderItems.cs
@@ -6,18 +6,18 @@ public struct OrderItems
     public int? IdOfOrder { get; set; }
 
     public int? IdOfProduct { get; set; }
-    
+
     public float? priceOfOneItem { get; set; }
 
     public int? amountOfItem { get; set; }
 
 
     public override string ToString() => $@"
-	Ordered product ID : {ID}, 
-	Order ID (Customer and shipping details) : {IdOfOrder}
-    Product ID : {IdOfProduct}
-    Price of one item : {priceOfOneItem}
-    the amount of items : {amountOfItem}
+	   Ordered product ID : {ID}, 
+	   Order ID (Customer and shipping details) : {IdOfOrder}
+       Product ID : {IdOfProduct}
+       Price of one item : {priceOfOneItem}
+       the amount of items : {amountOfItem}
 	";
 
 }

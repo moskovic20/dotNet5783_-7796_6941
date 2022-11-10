9a45fc59b36c3cdd272ac6e839f27da195f19178
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2022-11-10 16:18:15 +0200

    dalOrder

--- dotNet5783_-7796_6941/DelList/DelList/DalOrder.cs
+++ dotNet5783_-7796_6941/DelList/DelList/DalOrder.cs
@@ -3,31 +3,75 @@ using Do;
 
 namespace Dal;
 
-internal class DalOrder :IOrder
+internal class DalOrder : IOrder
 {
 
     public int Add(Order item)
     {
-        return 6;
+        try
+        {
+            Order myOrder = GetById(item.ID);
+            if (myOrder.IsDeleted == false)
+                throw new Exception("this item is already exists");
+
+
+
+
+        }
+        catch (Exception e)
+        {
+            if (e.Message == "this item is already exists")
+                throw new Exception("this item is already exists");
+        }
+
+        DataSource._Order.Add(item);
+        return item.ID;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        int indexOfOrderById = DataSource._Order.FindIndex(x => x.GetValueOrDefault().ID == id);
+
+        if (indexOfOrderById == -1)
+            throw new Exception("The order you wanted to delete is not found");
+
+
+        Order myOrder = DataSource._Order[indexOfOrderById].GetValueOrDefault();
+
+        if (myOrder.IsDeleted==true)
+            throw new Exception("The order you wanted to delete has already been deleted");
+
+
+        myOrder.IsDeleted = true;
+        DataSource._Order[indexOfOrderById] = (Order?)myOrder;
     }
 
     public IEnumerable<Order> GetAll()
     {
-        throw new NotImplementedException();
+        IEnumerable<Order?> allOrders= DataSource._Order.FindAll(x => true);
+        return (IEnumerable<Order>)allOrders;
     }
 
     public Order GetById(int id)
     {
-        throw new NotImplementedException();
+        Order? myOrder = DataSource._Order.Find(x => x.GetValueOrDefault().ID == id);
+
+        if (myOrder == null)
+            throw new Exception("The Order is not found");
+
+        return (Order)myOrder;
     }
 
     public void Update(Order item)
     {
-        throw new NotImplementedException();
+        try { GetById(item.ID); }
+
+        catch
+        {
+            throw new Exception("the order you wish to update does not exist");
+        }
+
+        Delete(item.ID);
+        Add(item);
     }
 }

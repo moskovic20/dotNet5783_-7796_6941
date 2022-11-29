6cf71dd10dfd10ce67a5ce332a0208c0387ec378
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2022-11-29 16:57:41 +0200

    לקבל שינויים של הילה

--- dotNet5783_-7796_6941/BL/BlApi/IOrder.cs
+++ dotNet5783_-7796_6941/BL/BlApi/IOrder.cs
@@ -5,4 +5,11 @@ namespace BlApi;
 public interface IOrder
 {
     IEnumerable<OrderForList> GetAllOrderForList();
+    BO.Order GetOrdertByID(int id);
+    BO.Order UpdateOrderShipping(int id);
+    BO.Order UpdateOrderDelivery(int id);
+    OrderTracking GetOrderTracking(int id);
+    void UpdateOrder(int id);
+
+
 }

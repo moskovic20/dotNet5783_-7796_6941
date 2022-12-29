ebd7324a916c0b75e7ad1152977f0aa956d0ced6
hilaaiz <haizenba@g.jct.ac.il>
2022-12-29 02:56:50 +0200

    pull trying

--- dotNet5783_-7796_6941/PL/Admin/AdminWindow.xaml.cs
+++ dotNet5783_-7796_6941/PL/Admin/AdminWindow.xaml.cs
@@ -9,7 +9,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using PL.PO;
-using PL.Products;
 using MaterialDesignThemes.Wpf;
 
 namespace PL.Admin
--- dotNet5783_-7796_6941/PL/PlEntity/Products/ProductListForM_Window.xaml.cs
+++ dotNet5783_-7796_6941/PL/PlEntity/Products/ProductListForM_Window.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BlApi;
+//using PL.BoProducts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,79 +16,76 @@ using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using PL.PO;
 
-namespace PL.Products
+namespace PL.Products;
+
+/// <summary>
+/// Interaction logic for ProductListForM_Window.xaml
+/// </summary>
+public partial class ProductListForM_Window : Window
 {
+    private IBl bl;
+    private ObservableCollection<ProductForList> allBooks;
 
-    /// <summary>
-    /// Interaction logic for ProductListForM_Window.xaml
-    /// </summary>
-    public partial class ProductListForM_Window : Window
+    public ProductListForM_Window(IBl bl)
     {
-        private IBl bl;
-        private ObservableCollection<ProductForList> allBooks;
+        InitializeComponent();
 
-        public ProductListForM_Window(IBl bl)
+        this.bl = bl;
+        allBooks = new();
+        allBooks = allBooks.ToObserCollection();
+        DataContext = allBooks;
+
+        // ProductListview.ItemsSource = bl.BoProduct.GetAllProductForList_forM();
+        cmbCategorySelector.ItemsSource = Enum.GetValues(typeof(BO.CATEGORY));
+
+    }
+
+
+    private void categoryFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        BO.CATEGORY? categ = cmbCategorySelector.SelectedItem as BO.CATEGORY?;
+
+        if (categ == BO.CATEGORY.all)
+            Products_DateGrid.ItemsSource = bl.BoProduct.GetListedProducts();
+        else
+            Products_DateGrid.ItemsSource = bl.BoProduct.GetListedProducts(BO.Filters.filterBYCategory, categ);
+    }
+
+    private void addButton_Click(object sender, RoutedEventArgs e)
+    {
+        new AddProductForM_Window(bl, allBooks).ShowDialog();
+    }
+
+    private void UpdatButton_Click(object sender, RoutedEventArgs e)
+    {
+        new UpdatProductForM_Window(bl, (ProductForList)Products_DateGrid.SelectedItem).ShowDialog();
+        DataContext = allBooks.ToObserCollection();
+    }
+
+
+    private void deleteProductButton_Click(object sender, RoutedEventArgs e)
+    {
+        PO.ProductForList pToD = (ProductForList)Products_DateGrid.SelectedItem;
+        try
         {
-            InitializeComponent();
-
-            this.bl = bl;
-            allBooks = new();
-            allBooks = allBooks.ToObserCollection();
-            DataContext = allBooks;
-
-            // ProductListview.ItemsSource = bl.BoProduct.GetAllProductForList_forM();
-            cmbCategorySelector.ItemsSource = Enum.GetValues(typeof(BO.CATEGORY));
-
-        }
-
-
-        private void categoryFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            BO.CATEGORY? categ = cmbCategorySelector.SelectedItem as BO.CATEGORY?;
-
-            if (categ == BO.CATEGORY.all)
-                Products_DateGrid.ItemsSource = bl.BoProduct.GetListedProducts();
-            else
-                Products_DateGrid.ItemsSource = bl.BoProduct.GetListedProducts(BO.Filters.filterBYCategory, categ);
-        }
-
-        private void addButton_Click(object sender, RoutedEventArgs e)
-        {
-            new AddProductForM_Window(bl, allBooks).ShowDialog();
-        }
-
-        private void UpdatButton_Click(object sender, RoutedEventArgs e)
-        {
-            new UpdatProductForM_Window(bl, (ProductForList)Products_DateGrid.SelectedItem).ShowDialog();
-            UpdatButton.IsEnabled = false;
-            Products_DateGrid.SelectedItem=null;
-        }
-
-
-        private void deleteProductButton_Click(object sender, RoutedEventArgs e)
-        {
-            PO.ProductForList pToD = (ProductForList)Products_DateGrid.SelectedItem;
-            try
+            var delete = MessageBox.Show("האם אתה בטוח שאתה רוצה למחוק ספר זה?", "מחיקת ספר", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            switch (delete)
             {
-                var delete = MessageBox.Show("האם אתה בטוח שאתה רוצה למחוק ספר זה?", "מחיקת ספר", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
-                switch (delete)
-                {
-                    case MessageBoxResult.Yes:
-                        bl.BoProduct.DeleteProductByID_forM(pToD.ID);
-                        allBooks.Remove(pToD);
-                        MessageBox.Show("!הספר נמחק בהצלחה");
-                        break;
-                    case MessageBoxResult.No:
-                        Products_DateGrid.SelectedItem = null;
-                        break;
-                }
+                case MessageBoxResult.Yes:
+                    bl.BoProduct.DeleteProductByID_forM(pToD.ID);
+                    allBooks.Remove(pToD);
+                    MessageBox.Show("!הספר נמחק בהצלחה");
+                    break;
+                case MessageBoxResult.No:
+                    Products_DateGrid.SelectedItem = null;
+                    break;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message + "\n" + ex.InnerException?.Message, "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK,
-                   MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading);
-            }
-
         }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message + "\n" + ex.InnerException?.Message, "שגיאה", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK,
+               MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading);
+        }
+
     }
 }
